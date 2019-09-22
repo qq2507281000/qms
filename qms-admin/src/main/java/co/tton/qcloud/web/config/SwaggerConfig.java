f683a008ea900137be7b3a21c405ee4cc7f4cb23
阎雨 <yanyu@tton.co>
2019-09-22 13:16:06 +0800

    Swagger配置更新

--- qms-admin/src/main/java/co/tton/qcloud/web/config/SwaggerConfig.java
+++ qms-admin/src/main/java/co/tton/qcloud/web/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package co.tton.qcloud.web.core.config;
+package co.tton.qcloud.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,9 +50,9 @@ public class SwaggerConfig
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 // 设置标题
-                .title("标题：若依管理系统_接口文档")
+                .title("QCLOUD接口文档")
                 // 描述
-                .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
+                .description("QCLOUD For 互动派")
                 // 作者信息
                 .contact(new Contact(Global.getName(), null, null))
                 // 版本
