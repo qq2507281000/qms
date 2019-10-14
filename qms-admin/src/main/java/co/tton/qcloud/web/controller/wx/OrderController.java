de205634b4701fd2ebd50368ef3eed957de3281c
xiguozheng <xiguozheng@tton.co>
2019-10-15 04:58:03 +0800

    冲突

--- qms-admin/src/main/java/co/tton/qcloud/web/controller/wx/OrderController.java
+++ qms-admin/src/main/java/co/tton/qcloud/web/controller/wx/OrderController.java
@@ -29,6 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -49,13 +50,109 @@ public class OrderController extends BaseController {
     private MinioFileService minioFileService;
     @Autowired
     private ITOrderUseEvaluationService tOrderUseEvaluationService;
+    @Autowired
+    private ITShopService shopService;
+    @Autowired
+    private ITShopCoursesService shopCoursesService;
+    @Autowired
+    private WxPayService wxService;
 
-//    @RequiresPermissions("wx:order:submit")
-//    @RequestMapping(value = "",method = RequestMethod.POST)
-//    public AjaxResult submitOrder(){
-//        //TODO:参数未定义，提交数据需要有实体对象。
-//        return null;
-//    }
+    //    @RequiresPermissions("wx:order:submit")
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public AjaxResult submitOrder(OrderModel model){
+        try{
+            if(model == null){
+                return AjaxResult.error("参数不允许为空。");
+            }
+            else{
+
+                String coursesName = "";
+                String shopAddress = "";
+                String shopName = "";
+//                String serialNo = "";
+
+                TShop shop = shopService.selectTShopById(model.getShopId());
+                shopName = shop.getName();
+                shopAddress = shop.getAddress();
+
+                TShopCourses courses = shopCoursesService.selectTShopCoursesById(model.getCoursesId());
+                coursesName = courses.getTitle();
+
+                String orderNo = StringUtils.genericOrderNo();
+                TOrder order = new TOrder();
+                String id = StringUtils.genericId();
+                order.setId(id);
+                order.setOrderNo(orderNo);
+                order.setSubject("互动派课程订单-" + coursesName);
+                order.setAddress(shopAddress);
+                order.setShopId(model.getShopId());
+                order.setShopName(shopName);
+                order.setPreRealPrice(model.getOrderPrice());
+                order.setPayPrice(model.getOrderPrice());
+                order.setPaymentChannel("wechatpay");
+                if(StringUtils.isNotEmpty(model.getCouponId())){
+                    order.setHaveDiscount(1);
+                }
+                else{
+                    order.setHaveDiscount(0);
+                }
+//                order.setSerialNo(serialNo);
+                order.setBookingTime(new Date());
+                order.setPayStatus("UNPAY");
+                order.setBillStatus("BOOKING");
+                order.setMemberId(model.getMemeberId());
+                order.setFlag(Constants.DATA_NORMAL);
+                int result = tOrderService.insertTOrder(order);
+                if(result == 1){
+                    AjaxResult ar = AjaxResult.success("订单提交成功。");
+                    ar.put("orderId",id);
+                    ar.put("orderNo",orderNo);
+                    ar.put("subject",order.getSubject());
+                    ar.put("price",model.getOrderPrice());
+                    return ar;
+                }
+                else{
+                    return AjaxResult.error("订单提交失败。");
+                }
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            logger.error("提交订单时发生异常。",ex);
+            return AjaxResult.error("提交订单时发生异常。");
+        }
+    }
+
+    @PostMapping("/pay")
+    @ApiOperation("订单支付")
+    public AjaxResult orderPay(OrderPayModel model){
+        try{
+            if(model == null){
+                return AjaxResult.error("订单支付参数不允许为空。");
+            }
+            else{
+                WxPayUnifiedOrderRequest request = new WxPayUnifiedOrderRequest();
+                request.setVersion("1.0");
+                request.setDeviceInfo("000000000000");
+                request.setBody(model.getSubject());
+                request.setAttach("课程订单");
+                request.setOutTradeNo(model.getOrderNo());
+                request.setTotalFee((int)(model.getPrice()*100));
+                request.setSpbillCreateIp(IpUtils.getHostIp());
+                request.setTimeStart(DateUtils.dateTimeNow());
+                request.setNotifyUrl(Global.getNotifyUrl());
+                request.setTradeType("JSAPI");
+                request.setOpenid(model.getOpenId());
+                wxService.createOrder(request);
+                return AjaxResult.success("支付中...");
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+            logger.error("订单支付时发生异常。",ex);
+            return AjaxResult.error("订单支付时发生异常。");
+        }
+    }
 
     /***
      *
@@ -72,32 +169,16 @@ public class OrderController extends BaseController {
         if(StringUtils.isNotEmpty(memberId)){
             TOrder tOrderNew = new TOrder();
             tOrderNew.setMemberId(memberId);
-            if(StringUtils.isNotEmpty(billStatus)&&billStatus.equals("FINISHED")){
+            if(StringUtils.isNotEmpty(billStatus)){
                 tOrderNew.setBillStatus(billStatus);
             }
-            if(StringUtils.isNotEmpty(payStatus)&&payStatus.equals("UNPAY")){
+            if(StringUtils.isNotEmpty(payStatus)){
                 tOrderNew.setPayStatus(payStatus);
             }
-            if(StringUtils.isNotEmpty(useStatus)&&useStatus.equals("UNUSED")){
+            if(StringUtils.isNotEmpty(useStatus)){
                 tOrderNew.setUseStatus(useStatus);
             }
             List<TOrder> tOrders =tOrderService.getOrderList(tOrderNew);
-            for(TOrder tOrder:tOrders) {
-                if (tOrder.getPayStatus().equals("UNPAY")) {
-                    tOrder.setWxStatus("未支付");
-                } else {
-                    tOrder.setWxStatus("待使用");
-                }
-                if (tOrder.getUseStatus().equals("USED")) {
-                    tOrder.setWxStatus("待评价");
-                }
-                if (tOrder.getUseStatus().equals("EXPIRED")) {
-                    tOrder.setWxStatus("已过期");
-                }
-                if (tOrder.getBillStatus().equals("FINISHED")) {
-                    tOrder.setWxStatus("已评价");
-                }
-            }
             return AjaxResult.success("获取分类成功。",tOrders);
         }else{
             return AjaxResult.error("参数错误");
@@ -126,45 +207,42 @@ public class OrderController extends BaseController {
     }
 
     //    @RequiresPermissions("wx:evaluation:insert")
-//    不好使
-    @RequestMapping(value = "/evaluation",method = RequestMethod.GET)
-    @ApiOperation("订单评价包含订单评价图片-----不好使")
+    @RequestMapping(value = "/evaluation",method = RequestMethod.POST)
+    @ApiOperation("订单和课程评价")
     public AjaxResult insertOrderUseEvaluation(@RequestParam(value = "orderno")String orderNo,
                                                @RequestParam(value = "memberid")String memberId,
                                                @RequestParam(value = "imageurl")String imageUrl,
                                                @RequestParam(value = "evaluation")String evaluation,
+                                               @RequestParam(value = "coursesid")String coursesId,
                                                @RequestParam(value = "star")String star){
-        try {
+        if(StringUtils.isNotEmpty(orderNo)&&StringUtils.isNotEmpty(memberId)){
             TOrderUseEvaluation tOrder = new TOrderUseEvaluation();
             String id = StringUtils.genericId();
             tOrder.setId(id);
-            if(tOrder.getParams().containsKey("file")){
-                //有新文件上传
-                MultipartFile file = (MultipartFile)tOrder.getParams().get("file");
-                if(file != null){
-                    String fileName = minioFileService.upload(file);
-                    tOrder.setImageUrl(fileName);
-                    tOrder.setFlag(Constants.DATA_NORMAL);
-                    tOrder.setOrderNo(orderNo);
-                    tOrder.setMemberId(memberId);
-                    tOrder.setImageUrl(imageUrl);
-                    tOrder.setEvaluation(evaluation);
-                    tOrder.setStar(star);
-                    int number = tOrderUseEvaluationService.insertOrderUseEvaluation(tOrder);
-                    return AjaxResult.success("数据保存成功。",number);
-                }
-                else{
-                    return AjaxResult.error("未能获取上传文件内容。");
-                }
+//            if(tOrder.getParams().containsKey("file")){
+            //调用上传图片接口
+
+//                MultipartFile file = (MultipartFile)tOrder.getParams().get("file");
+//                if(file != null){
+//                    String fileName = minioFileService.upload(file);
+//                    tOrder.setImageUrl(fileName);
+            tOrder.setFlag(Constants.DATA_NORMAL);
+            tOrder.setOrderNo(orderNo);
+            tOrder.setMemberId(memberId);
+            tOrder.setImageUrl(imageUrl);
+            tOrder.setEvaluation(evaluation);
+            if(!StringUtils.isNotEmpty(star)){
+                tOrder.setStar("0");
             }
-            else{
-                return AjaxResult.error("请选择图片上传。");
+            tOrder.setStar(star);
+            int number = tOrderUseEvaluationService.insertOrderUseEvaluation(tOrder);
+            if(number>0){
+                return AjaxResult.success("数据保存成功。",number);
+            }else{
+                return AjaxResult.success("数据保存失败。");
             }
-        }
-        catch(Exception ex){
-            ex.printStackTrace();
-            logger.error("保存图片时发生异常。",ex);
-            return AjaxResult.error("保存图片时发生异常。");
+        }else{
+            return AjaxResult.error("参数错误");
         }
     }
 
