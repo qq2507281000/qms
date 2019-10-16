6a177d4adfbaddecf5e1a7c59585766724174938
xiguozheng <xiguozheng@tton.co>
2019-10-16 10:06:04 +0800

    修改更新后不能启动bug，新增微信code接口

--- qms-admin/src/main/java/co/tton/qcloud/web/controller/wx/WxLoginController.java
+++ qms-admin/src/main/java/co/tton/qcloud/web/controller/wx/WxLoginController.java
@@ -5,19 +5,31 @@ import co.tton.qcloud.common.core.controller.BaseController;
 import co.tton.qcloud.common.core.domain.AjaxResult;
 import co.tton.qcloud.common.utils.StringUtils;
 import co.tton.qcloud.system.domain.TMember;
+import co.tton.qcloud.system.domain.WxOrderDetail;
 import co.tton.qcloud.system.service.ITMemberService;
+import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: qms
@@ -32,8 +44,10 @@ import java.util.List;
 @RequestMapping("/api/v1.0/login")
 @Api(value = "小程序获取Token和会员信息",tags="小程序获取Token和会员信息")
 public class WxLoginController extends BaseController {
+    private String path;
     @Autowired
     ITMemberService itMemberService;
+
     /**
      * <pre>
      * 获取用户信息和token接口
@@ -43,13 +57,13 @@ public class WxLoginController extends BaseController {
     @ApiOperation("返回个人信息和token")
 //    @RequiresPermissions("wx:member:openId")
     public AjaxResult<List> loginInfo(@RequestParam(value = "openId") String openId,
-                                      @RequestParam(value = "mobile",required = false) String mobile,
-                                      @RequestParam(value = "realName",required = false) String realName,
-                                      @RequestParam(value = "img",required = false) String img
-                                      ) {
-        if(StringUtils.isNotEmpty(openId)){
+                                      @RequestParam(value = "mobile", required = false) String mobile,
+                                      @RequestParam(value = "realName", required = false) String realName,
+                                      @RequestParam(value = "img", required = false) String img
+    ) {
+        if (StringUtils.isNotEmpty(openId)) {
             TMember tMember = itMemberService.loginInfo(openId);
-            if(StringUtils.isNull(tMember)){
+            if (StringUtils.isNull(tMember)) {
                 //如果查询不到就插入
                 TMember tMemberOne = new TMember();
                 tMemberOne.setId(StringUtils.genericId());
@@ -58,13 +72,66 @@ public class WxLoginController extends BaseController {
                 tMemberOne.setWxName(realName);
                 tMemberOne.setImg(img);
                 int number = itMemberService.insertloginInfo(tMemberOne);
-                if(number>0){
-                    return AjaxResult.success("新增用户插入成功。",number);
+                if (number > 0) {
+                    return AjaxResult.success("新增用户插入成功。", number);
                 }
             }
-            return AjaxResult.success("获取信息成功。",tMember);
-          }else{
+            return AjaxResult.success("获取信息成功。", tMember);
+        } else {
             return AjaxResult.error("openId错误。");
+        }
+    }
+
+    //    @RequiresPermissions("wx:order:detail")
+    @RequestMapping(value = "/codeww", method = RequestMethod.GET)
+    @ApiOperation("获取订单详情")
+    public AjaxResult<WxOrderDetail> getCode(@RequestParam("code") String code) {
+        if (StringUtils.isNotEmpty(code)) {
+            return null;
+        } else {
+            return AjaxResult.error("报错：code为空。");
+        }
+    }
+
+    //    @RequiresPermissions("wx:code)
+    @RequestMapping(value = "/code", method = RequestMethod.GET)
+    @ApiOperation("获取code")
+    public AjaxResult<Map> outSideCall(@RequestParam("code") String code) throws Exception {
+        if (StringUtils.isNotEmpty(code)) {
+            InputStream is = null;
+            String body = null;
+            StringBuilder res = new StringBuilder();
+            Map returnMap = new HashMap<>();
+            String url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx979a25003d5b4c1b&secret=13da5ae620cba1a98e1f81e8241946f5&js_code=" + code + "&grant_type=authorization_code";
+            HttpGet get = new HttpGet(url);
+            CloseableHttpClient client = HttpClients.createDefault();
+            CloseableHttpResponse response = null;
+            try {
+                response = client.execute(get);
+                RequestConfig config = RequestConfig.custom().setConnectTimeout(5000).build();
+                get.setConfig(config);
+                HttpEntity entity = response.getEntity();
+
+                if (entity != null) {
+                    is = entity.getContent();
+                    //转换为字节输入流
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is, Consts.UTF_8));
+
+                    while ((body = br.readLine()) != null) {
+                        res.append(body);
+                    }
+                }
+                returnMap = (Map) JSON.parse(res.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(StringUtils.isNull(returnMap)){
+                return AjaxResult.error("失败",returnMap);
+            }else{
+                return AjaxResult.success("成功",returnMap);
+            }
+        } else {
+            return AjaxResult.error("报错：code为空。");
         }
     }
 
